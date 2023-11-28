2c4e7c054265d30052ce67a9033ac918d9dd567a
林千里 <lincheney@gmail.com>
2023-11-28 18:06:15 +1100

    fix issues where multiple matches on the same line

--- lua/multiple-cursors/init.lua
+++ lua/multiple-cursors/init.lua
@@ -81,21 +81,19 @@ function M.start_at_regex(regex, replace, range)
     -- get all substitute matches
     vim.g.multiple_cursors_positions = {}
 
-    local cmd = 's//\\=len(add(g:multiple_cursors_positions, [getcurpos(), submatch(0, 1)]))'
+    local cmd = 's//\\=len(add(g:multiple_cursors_positions, [getcurpos(), submatch(0, 1)]))/n'
     if range then
         cmd = range[1]..','..range[2]..cmd
     else
         cmd = '%'..cmd
     end
 
-    if replace then
-        vim.cmd(cmd..' ? "" : ""')
-    else
-        vim.cmd(cmd..'/n')
+    if not pcall(function() vim.cmd(cmd) end) then
+        return
     end
     vim.cmd[[nohlsearch]]
 
-    local jumpto = nil
+    local index = nil
     -- get all substitute matches
     local positions = {}
     local visuals = {}
@@ -105,27 +103,21 @@ function M.start_at_regex(regex, replace, range)
         local lines = value[2]
         local finish = {start[1] + #lines - 1, #lines == 1 and start[2]+#lines[1] or #lines[#lines]}
 
-        if replace then
-            table.insert(positions, start)
-        else
-            table.insert(positions, {finish[1], finish[2]-1})
-            table.insert(visuals, {start[1], start[2], finish[1], finish[2]})
-        end
+        table.insert(positions, {finish[1], finish[2]-1})
+        table.insert(visuals, {start[1], start[2], finish[1], finish[2]})
 
-        if vim.version.cmp(cursor, positions[i]) <= 0 and (not jumpto or vim.version.cmp(positions[i], jumpto) < 0) then
-            jumpto = positions[i]
+        if vim.version.cmp(cursor, positions[i]) <= 0 and (not index or vim.version.cmp(positions[i], positions[index]) < 0) then
+            index = i
         end
     end
-    jumpto = jumpto or positions[1]
+    index = index or 1
+
+    utils.set_visual_range(visuals[index], {positions[index][1], positions[index][2]+1}, 'v')
+    M.start(positions, visuals)
 
     if replace then
-        vim.api.nvim_win_set_cursor(0, {jumpto[1]+1, jumpto[2]})
-        M.start(positions)
-        vim.cmd[[startinsert]]
+        vim.api.nvim_feedkeys('c', 't', false)
         utils.wait_for_normal_mode(M.stop)
-    else
-        utils.set_visual_range(visuals[index], {positions[index][1], positions[index][2]+1}, 'v')
-        M.start(positions, visuals)
     end
 end
 
